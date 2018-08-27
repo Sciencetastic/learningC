ee2d1abeb2ad6fc7a0ed7e5573e660f043014d4d
William A. McDonald <mcdonaldwilliama@gmail.com>
2018-08-27 01:59:28 -0700

    Fixed project 3 in Make: Jumpstarting C book
    Wrote the code from the chapter 3 of the book Jumpstarting C and it errored, for some reason the writer tried to make functions inside of functions...  Anyway, my friend John showed me how to fix it.

--- main.c
+++ main.c
@@ -1,24 +1,79 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+* To change this license header, choose License Headers in Project Properties.
+* To change this template file, choose Tools | Templates
+* and open the template in the editor.
+*/
 
 /* 
- * File:   main.c
- * Author: William Arthur McDonald
- *
- * Created on August 25, 2018, 9:43 PM
- */
+* File:   main.c
+* Author: William Arthur McDonald
+*
+* Created on August 25, 2018, 9:43 PM
+*/
 
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
+
+
+double circle(int r);
+double square(int h, int w);
+double triangle(int b, int h);
+
 
 /*
- * 
- */
-int main(int argc, char** argv) {
+* 
+*/
+void main()
+{
+    int x, y;
+    double area;
+    char shape[10];
 
-    return (EXIT_SUCCESS);
+    printf("What shape would you like to find the area of?\n");
+    scanf("%s", &shape);
+
+    if (strcmp(shape, "circle") == 0)
+    {
+        printf("Enter radius: ");
+        scanf("%d", &x);
+        area = circle(x);
+    }
+    else if (strcmp(shape, "rectangle") == 0 || strcmp(shape, "square") == 0)
+    {
+        printf("Enter height: ");
+        scanf("%d", &x);
+        printf("Enter width: ");
+        scanf("%d", &y);
+        area = square(x,y);
+    }
+    else if (strcmp(shape, "triangle") == 0)
+    {
+        printf("Enter base: ");
+        scanf("%d", &x);
+        printf("Enter height: ");
+        scanf("%d", &y);
+        area = triangle(x,y);
+    }
+    else
+    {
+        printf("Sorry, I don't recognize that shape.\n");
+        return;
+    }
+
+    printf ("The area of %s is %f\n", shape, area);
 }
 
+double circle(int r)
+{
+    return 3.14159 * r *r;
+}
+
+double square(int h, int w)
+{
+    return h * w;
+}          
+
+double triangle(int b, int h)
+{
+    return 0.5 * b * h;
+}
